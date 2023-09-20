310b2f7f209caf30d720ff4d116bd7dbbeb79276
@deepankan1 <deepankan@ramailo.tech>
2023-09-20 17:31:49 +0530

    inital commit

---
+++ src/main/java/com/example/spring/Idea2MvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idea2MvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idea2MvpApplication.class, args);
+	}
+
+}
